63040d5a2d58583e7ae3417ba3494d5f2c3a6b30
yomiyuarts <tenten20.mc@gmail.com>
2026-08-08 14:12:49 +0800

    modal animation

--- JS/settings.js
+++ JS/settings.js
@@ -1,19 +1,18 @@
+const settingsButton = document.getElementById("settingsButton");
+const settingsModal = document.getElementById("settingsModal");
 
-    const settingsButton = document.getElementById("settingsButton");
-    const settingsModal = document.getElementById("settingsModal");
+settingsButton.onclick = function () {
+    settingsModal.classList.add("active");
+};
 
-    settingsButton.onclick = function () {
-        settingsModal.classList.add("active");
-    };
+settingsModal.onclick = function (event) {
+    if (event.target === settingsModal) {
+        settingsModal.classList.remove("active");
+    }
+};
 
-    settingsModal.onclick = function (event) {
-        if (event.target === settingsModal) {
-            settingsModal.classList.remove("active");
-        }
-    };
-
-    document.addEventListener("keydown", function (event) {
-        if (event.key === "Escape") {
-            settingsModal.classList.remove("active");
-        }
-    });
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+        settingsModal.classList.remove("active");
+    }
+});
